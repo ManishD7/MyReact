e7bd211152c6ce40b3ba1f87755ca4414e4d4849
ManishD7 <50623561+ManishD7@users.noreply.github.com>
2020-10-07 15:42:02 +0530

    Add files via upload

---
+++ precache-manifest.d3644d6f0946c54aeaae260e0b2882b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c298981828e48c7369f0970d5ee1c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97e92dda21342535059",
+    "url": "/static/css/2.49e9c873.chunk.css"
+  },
+  {
+    "revision": "8befb2422f1d86037b51",
+    "url": "/static/css/main.ca4795a4.chunk.css"
+  },
+  {
+    "revision": "e97e92dda21342535059",
+    "url": "/static/js/2.448f6aed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.448f6aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8befb2422f1d86037b51",
+    "url": "/static/js/main.82e0f587.chunk.js"
+  },
+  {
+    "revision": "d5e62d8252e06edc47b4",
+    "url": "/static/js/runtime-main.ea6d5ea9.js"
+  }
+]);
